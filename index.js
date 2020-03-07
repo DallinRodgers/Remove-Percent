1502898efe4eaebc49304a7ac888c4663208865f
Dallin Rodgers <rodgers.dallin@gmail.com>
2020-03-07 10:03:27 -0700

    Update index.js
    Working on removePercent. It works when there is a single  '%'. If it has repeated '%%%' it does not work as intended

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
-const erase = word => {
-  let checkThisWord = word;
+const erase = (word) => {
+  let checkThisWord = word.split("");
   // Check for any letters
   if (checkForLetters(checkThisWord)) {
-   let theseIndexs = getPercentIndex(checkThisWord);
-   console.log(theseIndexs);
-   removePercent(checkThisWord, theseIndexs);
+    let theseIndexs = getPercentIndex(checkThisWord);
+    // console.log(theseIndexs);
+    removePercent(checkThisWord, theseIndexs);
   } else {
-    console.log("\"\"");
+    console.log('""');
   }
 };
 
-const getPercentIndex = word => {
-  const thisWord = word.split("");
+const removePercent = (thisWord, removeHere) => {
+  let newWord = thisWord;
+  // console.log(thisWord);
+  // console.log(removeHere);
+  for (let i = thisWord.length - 1; i >= 0; i--) {
+    if (removeHere.includes(i)) {
+      // console.log(`found a index to remove here: ${i}`);
+      if (i === 0) {
+        // console.log(newWord);
+        newWord.splice(i, 1);
+        // console.log("first character");
+        // console.log(newWord);
+      } else {
+        // console.log(i);
+        newWord.splice(i - 1, 2);
+        // console.log("first character");
+        // console.log(newWord);
+      }
+    }
+  }
+  // for (let i = 0; i < newWord.length; i++) {
+  //   if (removeHere.includes(i)) {
+  //     // console.log(`found a index to remove here: ${i}`);
+  //     if (i === 0) {
+  //       // console.log(newWord);
+  //       newWord.splice(i, 1);
+  //       // console.log("first character");
+  //       // console.log(newWord);
+  //     } else {
+  //       // console.log(i);
+  //       newWord.splice(i - 1, 2);
+  //       // console.log("first character");
+  //       // console.log(newWord);
+  //     }
+  //   }
+  // }
+  newWord = newWord.join("");
+  console.log(newWord);
+};
+
+const getPercentIndex = (word) => {
   let percentHere = [];
-  for(let i = 0; i < thisWord.length; i++){
-    if(thisWord[i] === "%"){
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === "%") {
       percentHere.push(i);
-      // console.log(i)
-      // if(i === 0){
-      //   // console.log(newWord);
-      //   newWord.splice(i, 1);
-      //   // console.log("first character");
-      //   // console.log(newWord);
-      // } else {
-      //   // console.log(i);
-      //   newWord.splice((i - 1), 2);
-      //   // console.log("first character");
-      //   // console.log(newWord);
-      // }
     }
   }
   return percentHere;
-}
+};
 
-const checkForLetters = word => {
+const checkForLetters = (word) => {
   let anyLetters = false;
   for (let i = 0; i < word.length; i++) {
     if (word[i] !== "%") {
@@ -42,6 +69,16 @@ const checkForLetters = word => {
   }
   return anyLetters;
 };
-// erase("%apple");
-erase("%ap%p%le");
-// erase("%%%%");
+
+erase("he%%l%hel%llo");
+erase("major% spar%%ks");
+erase("si%%%t boy");
+erase("%%%%");
+
+// erase("he%%l%hel%llo") ➞ "hello"
+
+// erase("major% spar%%ks") ➞ "majo spks"
+
+// erase("si%%%t boy") ➞ "t boy"
+
+// erase("%%%%") ➞ ""
